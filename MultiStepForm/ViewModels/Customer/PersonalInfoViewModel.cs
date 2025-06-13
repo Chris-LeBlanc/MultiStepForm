e5d756a26b4a499db9c9048ef4fe297d8faffcd8
Chris-LeBlanc <146110323+Chris-LeBlanc@users.noreply.github.com>
2025-06-13 00:20:10 -0300

    Implemented views and data annotations for user input

--- MultiStepForm/ViewModels/Customer/PersonalInfoViewModel.cs
+++ MultiStepForm/ViewModels/Customer/PersonalInfoViewModel.cs
@@ -1,11 +1,19 @@
-﻿namespace MultiStepForm.Web.ViewModels.Customer
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiStepForm.Web.ViewModels.Customer
 {
     public class PersonalInfoViewModel
     {
+        [Required]
+        [StringLength(3, ErrorMessage = "Name must be at least 3 characters in length")]
         public string? Name { get; set; }
 
+        [Required]
+        [RegularExpression(@"^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,4})$", ErrorMessage = "Email is invalid format")]
         public string? Email {  get; set; }
 
+        [Required]
+        [RegularExpression(@"\(\d{3}\) \d{3}-\d{4}", ErrorMessage = "Phone number must match the pattern: (NNN) NNN-NNNN")]
         public string? PhoneNumber { get; set; }
     }
 }
