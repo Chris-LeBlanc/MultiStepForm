b8366894467731392cbbee516c483d419cd10efc
Chris-LeBlanc <146110323+Chris-LeBlanc@users.noreply.github.com>
2025-06-29 15:35:35 -0300

    Made adjustments to Plan View Model

--- MultiStepForm/ViewModels/Customer/PlanSelectViewModel.cs
+++ MultiStepForm/ViewModels/Customer/PlanSelectViewModel.cs
@@ -1,10 +1,14 @@
-﻿using MultiStepForm.Types;
+﻿using System.ComponentModel.DataAnnotations;
+using MultiStepForm.Types;
 
 namespace MultiStepForm.Web.ViewModels.Customer
 {
     public class PlanSelectViewModel
     {
-        public Plan Plans { get; set; }
+        [Required]
+        public List<Plan>? Plans { get; set; }
+
+        public Plan SelectedPlan { get; set; }
 
         public BillingType BillingType { get; set; }
     }
